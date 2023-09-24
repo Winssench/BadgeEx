1f45637dd6e1facf2e2d19a59cacf374eb367a86
Winssench <omar.chichaoui@gmail.com>
2023-09-24 11:26:11 +0200

    verifying size of the badge

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# BadgeEx
-this project verifies the validity of different badges uploaded by users
+# Image Verification Python Project
+
+This project demonstrates how to verify badge images using Python. It includes a verification function and an example script.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- **Python**: You need Python 3.x installed on your system. If it's not installed, you can download it from the [official Python website](https://www.python.org/downloads/).
+
+## Setup Instructions
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/image-verification-project.git
+   cd image-verification-project
--- verify_badge.py
+++ verify_badge.py
@@ -3,5 +3,52 @@ Author: Omar CHICHAOUI
 Date: September 24, 2023
 Description: This is a Python script to verify badges.
 """
+from PIL import Image, ImageDraw
 
-# Your Python code goes here
+def verify_badge(image_path):
+    """
+    Verify a badge image.
+
+    Parameters:
+        image_path (str): The path to the badge image file to be verified.
+
+    Returns:
+        str: A string describing the verification result.
+
+    This function verifies the specified badge image to ensure it meets certain criteria.
+    - `image_path` should be the path to the badge image file you want to verify.
+
+    The function returns a string describing the verification result, which can be one of the following:
+    - "Image size is not 512x512" if the image size is incorrect.
+
+    Example:
+    result = verify_badge("valid-badge.png")
+    print(result)  # Outputs: "Badge is valid"
+    """
+
+    try:
+        # Open the image
+        img = Image.open(image_path)
+
+        # Verify size (512x512)
+        if img.size != (512, 512):
+            return "Image size is not 512x512"
+
+
+        # Check if the non-transparent pixels are within a circle
+   
+
+        # Color analysis (define your criteria for "happy" colors)
+        # You can use the Image.getpixel() method to analyze individual pixels
+
+        return "Badge is valid"
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+
+invalid_image = 'invalid_badge.png'
+# Verify an invalid badge image
+result_invalid = verify_badge(invalid_image)
+print(f"Verification result for {invalid_image}: {result_invalid}")
